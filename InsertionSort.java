71ca8a85efc6f7a9a9a29b936fcf2a8773d34d28
barry798 <huang.rui9@northeastern.edu>
2025-01-22 16:35:14 -0500

    Add files via upload

---
+++ InsertionSort.java
@@ -0,0 +1,30 @@
+public class InsertionSort {
+    //Sorts the array using Insertion Sort.
+    public void sort(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) {
+            insert(i, a);
+        }
+    }
+
+    private void insert(int i, Comparable[] a) {
+        //Inserts the 'Transition element' into its correct position in the sorted portion of the array.
+        //TODO: TO BE IMPLEMENTED
+    	int n = a.length;
+    	
+    	for(i=1;i<n;i++) {
+    		int key = (int) a[i];
+    		int j= i-1;
+    	while(j>=0 && (int)a[j] > key) {
+    		a[j+1] = a[j];
+    		j--;
+    	}
+    	a[j+1] = key;
+    	}
+    }
+
+    private void swap(Object[] a, int i, int j) {
+        Object temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+}
